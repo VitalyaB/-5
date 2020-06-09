eac4844f3c7ab207641d5dba2e071ffd6f834a4c
VitalyaB <66676005+VitalyaB@users.noreply.github.com>
2020-06-09 16:23:24 +0500

    Add files via upload

---
+++ ООП 5/ООП 5/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ООП_5
+{
+    class Person
+    {
+        private string name;
+        private string surname;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Name + " " + Surname);
+        }
+
+        public Person(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+    }
+
+    class Employee : Person
+    {
+        private string company;
+
+        public Employee(string name, string surname, string comp)
+            : base(name, surname)
+        {
+            company = comp;
+        }
+
+        public string Company
+        {
+            get { return company; }
+            set { company = value; }
+        }
+        public void DisplayE()
+        {
+            Console.WriteLine(Name + " " + Surname + " " + Company);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Марат", "Иванов");
+            Employee emp1 = new Employee("Сергей", "Сергеевич", "Молодец");
+
+            person1.Display();
+            emp1.DisplayE();
+            Console.ReadKey();
+        }
+    }
+}
